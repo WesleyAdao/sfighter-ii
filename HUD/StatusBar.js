ccd777f7e621d6daef7d3510a623c329b86b5f70
Wesley Adao <wesleyadao88@hotmail.com>
2023-10-27 12:22:59 -0300

    Feature: states globais aplicadas à barra de status

--- HUD/StatusBar.js
+++ HUD/StatusBar.js
@@ -1,4 +1,5 @@
-import { TIME_DELAY, TIME_FLASH_DELAY, TIME_FRAME_KEYS } from "../constants/battle.js";
+import { HEALTH_DAMAGE_COLOR, HEALTH_MAX_HIT_POINTS, TIME_DELAY, TIME_FLASH_DELAY, TIME_FRAME_KEYS } from "../constants/battle.js";
+import { FPS } from "../constants/game.js";
 import { gameState } from "../states/gameState.js";
 import { drawFrame } from "../utils/context.js";
 
@@ -11,53 +12,61 @@ export class StatusBar {
         this.timeFlashTimer = 0;
         this.useFlashFrames = false;
 
+        this.healthBars = [{
+            timer: 0,
+            hitPoints: HEALTH_MAX_HIT_POINTS,
+        }, {
+            timer: 0,
+            hitPoints: HEALTH_MAX_HIT_POINTS,
+        }];
+
         this.frames = new Map([
             ['health-bar', [16, 18, 145, 11]],
 
             ['ko-white', [161, 16, 32, 14]],
 
-            [`${TIME_FRAME_KEYS[0]}-0`, [16, 32, 14, 16]],
-            [`${TIME_FRAME_KEYS[0]}-1`, [32, 32, 14, 16]],
-            [`${TIME_FRAME_KEYS[0]}-2`, [48, 32, 14, 16]],
-            [`${TIME_FRAME_KEYS[0]}-3`, [64, 32, 14, 16]],
-            [`${TIME_FRAME_KEYS[0]}-4`, [80, 32, 14, 16]],
-            [`${TIME_FRAME_KEYS[0]}-5`, [96, 32, 14, 16]],
+            [`${TIME_FRAME_KEYS[0]}-0`, [16,  32, 14, 16]],
+            [`${TIME_FRAME_KEYS[0]}-1`, [32,  32, 14, 16]],
+            [`${TIME_FRAME_KEYS[0]}-2`, [48,  32, 14, 16]],
+            [`${TIME_FRAME_KEYS[0]}-3`, [64,  32, 14, 16]],
+            [`${TIME_FRAME_KEYS[0]}-4`, [80,  32, 14, 16]],
+            [`${TIME_FRAME_KEYS[0]}-5`, [96,  32, 14, 16]],
             [`${TIME_FRAME_KEYS[0]}-6`, [112, 32, 14, 16]],
             [`${TIME_FRAME_KEYS[0]}-7`, [128, 32, 14, 16]],
             [`${TIME_FRAME_KEYS[0]}-8`, [144, 32, 14, 16]],
             [`${TIME_FRAME_KEYS[0]}-9`, [160, 32, 14, 16]],
 
-            [`${TIME_FRAME_KEYS[1]}-0`, [16, 192, 14, 16]],
-            [`${TIME_FRAME_KEYS[1]}-1`, [32, 192, 14, 16]],
-            [`${TIME_FRAME_KEYS[1]}-2`, [48, 192, 14, 16]],
-            [`${TIME_FRAME_KEYS[1]}-3`, [64, 192, 14, 16]],
-            [`${TIME_FRAME_KEYS[1]}-4`, [80, 192, 14, 16]],
-            [`${TIME_FRAME_KEYS[1]}-5`, [96, 192, 14, 16]],
+            [`${TIME_FRAME_KEYS[1]}-0`, [16,  192, 14, 16]],
+            [`${TIME_FRAME_KEYS[1]}-1`, [32,  192, 14, 16]],
+            [`${TIME_FRAME_KEYS[1]}-2`, [48,  192, 14, 16]],
+            [`${TIME_FRAME_KEYS[1]}-3`, [64,  192, 14, 16]],
+            [`${TIME_FRAME_KEYS[1]}-4`, [80,  192, 14, 16]],
+            [`${TIME_FRAME_KEYS[1]}-5`, [96 , 192, 14, 16]],
             [`${TIME_FRAME_KEYS[1]}-6`, [112, 192, 14, 16]],
             [`${TIME_FRAME_KEYS[1]}-7`, [128, 192, 14, 16]],
             [`${TIME_FRAME_KEYS[1]}-8`, [144, 192, 14, 16]],
             [`${TIME_FRAME_KEYS[1]}-9`, [160, 192, 14, 16]],
 
             //Numeric
-            ['score-0', [17, 101, 10, 10]],
-            ['score-1', [29, 101, 10, 10]],
-            ['score-2', [41, 101, 10, 10]],
-            ['score-3', [53, 101, 10, 10]],
-            ['score-4', [65, 101, 10, 10]],
-            ['score-5', [77, 101, 10, 10]],
-            ['score-6', [89, 101, 10, 10]],
+            ['score-0', [17,  101, 10, 10]],
+            ['score-1', [29,  101, 10, 10]],
+            ['score-2', [41,  101, 10, 10]],
+            ['score-3', [53,  101, 10, 10]],
+            ['score-4', [65,  101, 10, 10]],
+            ['score-5', [77,  101, 10, 10]],
+            ['score-6', [89,  101, 10, 10]],
             ['score-7', [101, 101, 10, 10]],
             ['score-8', [113, 101, 10, 10]],
             ['score-9', [125, 101, 10, 10]],
 
             //Alpha
-            ['score-@', [17, 113, 10, 10]],
-            ['score-A', [29, 113, 12, 10]],
-            ['score-B', [41, 113, 10, 10]],
-            ['score-C', [53, 113, 10, 10]],
-            ['score-D', [65, 113, 10, 10]],
-            ['score-E', [77, 113, 10, 10]],
-            ['score-F', [89, 113, 10, 10]],
+            ['score-@', [17,  113, 10, 10]],
+            ['score-A', [29,  113, 12, 10]],
+            ['score-B', [41,  113, 10, 10]],
+            ['score-C', [53,  113, 10, 10]],
+            ['score-D', [65,  113, 10, 10]],
+            ['score-E', [77,  113, 10, 10]],
+            ['score-F', [89,  113, 10, 10]],
             ['score-G', [101, 113, 10, 10]],
             ['score-H', [113, 113, 10, 10]],
             ['score-I', [125, 113, 10, 10]],
@@ -67,13 +76,13 @@ export class StatusBar {
             ['score-M', [173, 113, 10, 10]],
             ['score-N', [185, 113, 10, 10]],
             ['score-O', [197, 113, 10, 10]],
-            ['score-P', [17, 125, 10, 10]],
-            ['score-Q', [29, 125, 10, 10]],
-            ['score-R', [41, 125, 10, 10]],
-            ['score-S', [53, 125, 10, 10]],
-            ['score-T', [65, 125, 10, 10]],
-            ['score-U', [77, 125, 10, 10]],
-            ['score-V', [89, 125, 10, 10]],
+            ['score-P', [17,  125, 10, 10]],
+            ['score-Q', [29,  125, 10, 10]],
+            ['score-R', [41,  125, 10, 10]],
+            ['score-S', [53,  125, 10, 10]],
+            ['score-T', [65,  125, 10, 10]],
+            ['score-U', [77,  125, 10, 10]],
+            ['score-V', [89,  125, 10, 10]],
             ['score-W', [101, 125, 10, 10]],
             ['score-X', [113, 125, 10, 10]],
             ['score-Y', [125, 125, 10, 10]],
@@ -81,7 +90,7 @@ export class StatusBar {
 
             //Name Tags
             ['tag-ken', [128, 56, 30, 9]],
-            ['tag-ryu', [16, 56, 28, 9]],
+            ['tag-ryu', [16,  56, 28, 9]],
         ]);
 
         this.names = gameState.fighters.map(({id}) => `tag-${id.toLowerCase()}`);
@@ -106,14 +115,35 @@ export class StatusBar {
         }
     }
 
+    updateHeatlBars(time) {
+        for (const index in this.healthBars) {
+            if (this.healthBars[index].hitPoints <= gameState.fighters[index].hitPoints) continue;
+            this.healthBars[index].hitPoints = Math.max(0, this.healthBars[index].hitPoints - (time.secondsPassed * FPS));
+        }
+    }
+
     update(time) {
         this.updateTime(time);
+        this.updateHeatlBars(time);
     }
 
     drawHeatlBars(context) {
-        this.drawFrame(context, 'health-bar', 31, 20);
-        this.drawFrame(context, 'ko-white', 176, 18);
+        this.drawFrame(context, 'health-bar', 31,  20);
+        this.drawFrame(context, 'ko-white',   176, 18);
         this.drawFrame(context, 'health-bar', 353, 20, -1);
+
+        context.fillStyle = HEALTH_DAMAGE_COLOR;
+
+        context.beginPath();
+        context.fillRect(
+            32, 21,
+            HEALTH_MAX_HIT_POINTS - Math.floor(this.healthBars[0].hitPoints), 9,
+        );
+
+        context.fillRect(
+            208 + Math.floor(this.healthBars[1].hitPoints), 21,
+            HEALTH_MAX_HIT_POINTS - Math.floor(this.healthBars[1].hitPoints), 9,
+        );
     }
 
     drawNameTags(context) {
@@ -147,14 +177,14 @@ export class StatusBar {
     }
 
     drawScores(context) {
-        this.drawScoreLabel(context, 'P1', 4);
-        this.drawScore(context, 1, 45);
+        this.drawScoreLabel(context, '1P', 4);
+        this.drawScore(context, gameState.fighters[0].score, 45);
         
         this.drawScoreLabel(context, 'KHA', 133);
         this.drawScore(context, 50000, 177);
 
-        this.drawScoreLabel(context, 'P2', 269);
-        this.drawScore(context, 1, 309);
+        this.drawScoreLabel(context, '2P', 269);
+        this.drawScore(context, gameState.fighters[1].score, 309);
         
     }
 
